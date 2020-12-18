8b3b0f7c598a4722a225dadf17f97f5ff33bad02
sokudon <tdty15@gmail.com>
2020-12-18 23:33:33 +0900

    C#+python

---
+++ miri_2020-12-18.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import time
+import datetime
+import math
+#ぱいそんのたいまー
+
+root = Tk()
+
+buff = StringVar()
+buff.set('')
+Label(textvariable = buff).pack()
+
+
+ibe='カーテシーフラワー'
+s = '2020-12-18T14:00:00+08:00'
+ss = '2020-12-24T12:00:00Z'
+timefm='%Y/%m/%d %H:%M:%S %Z'
+
+s=s.replace('Z', '+00:00')
+ss=ss.replace('Z', '+00:00')
+
+st  = datetime.datetime.fromisoformat(s)
+en  = datetime.datetime.fromisoformat(ss)
+
+sttmp=st.timestamp()
+st=datetime.datetime.fromtimestamp(sttmp)
+ts = st.strftime(timefm)
+entmp=en.timestamp()
+en=datetime.datetime.fromtimestamp(entmp)
+te = en.strftime(timefm)
+span= abs(en-st)
+
+
+def dtime(dt):
+    if dt<0:
+            return "0日0時間0分"
+    dt=abs(dt)
+    seconds  = math.floor((dt / 10) % 60)
+    minutes  = math.floor((dt / 60) % 60)
+    hours    = math.floor((dt / 3600) % 24)
+    days     = math.floor(dt / 86400)
+    tmp = str(days) +"日" +str(hours)+"時間"+str(minutes) +"分"
+    return tmp
+
+# 時刻の表示
+def show_time():
+    nn=time.time()
+    elapsed=dtime(nn-sttmp)
+    left= dtime(entmp-nn)
+    x = abs(nn-sttmp)/abs(entmp-sttmp)*100
+    n = 2
+    y = math.floor(x * 10 ** n) / (10 ** n)
+    progress = y
+    if y>100:
+             y=100
+    tm=time.strftime(timefm)
+    buff.set(ibe +'\n'+ tm+'\n' +ts +'\n'+te+"\n" +str(span)+"\n"+elapsed+"\n"+left+"\n"+str(y)+"%")
+    root.after(1000, show_time)
+    #1000msの後にshow_time関数を呼び出しています
+
+
+show_time()
+root.mainloop()
